7328d81c2a8eaf378a0062dd7bab8ffdd0f99a38
tea35 <blackapple3511@gmail.com>
2026-05-21 17:55:34 +0900

    fix: headerの固定

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 w-full bg-background border-b border-card">
+    <header className="fixed top-0 left-0 right-0 z-[100] w-full bg-background border-b border-card">
       <div className="w-full mx-auto px-4 py-4 flex items-center">
         <h1 className="text-2xl font-bold">
           <Link href="#home" className="no-underline text-foreground">
